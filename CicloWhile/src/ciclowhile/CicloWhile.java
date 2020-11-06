8c94386062ec75b4be2d30b5e17c8c794319174a
IA-RG <alexgamesrg@gmail.com>
2020-11-05 23:24:35 -0600

    FundamentosJava
    5/11/2020

--- CicloWhile/src/ciclowhile/CicloWhile.java
+++ CicloWhile/src/ciclowhile/CicloWhile.java
@@ -6,7 +6,10 @@ package ciclowhile;
 
 public class CicloWhile {
     public static void main(String[] args) {
-        
-    }
-    
+        var contador = 0;
+        while(contador < 3){
+            System.out.println("contador = " + contador);
+            contador++;
+        }
+    } 
 }
